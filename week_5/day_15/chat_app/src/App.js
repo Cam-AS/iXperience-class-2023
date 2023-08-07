854221ec8805c228568ffe2a8faf6d614c82ee25
Cameron Kirkpatrick <cameronk@appstrax.tech>
2023-08-07 15:56:30 +0200

    Added day_15 new project

---
+++ week_5/day_15/chat_app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// import the bootstrap items
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+  return (
+    <div className="text-center mt-5">
+      <h1>Chat Application</h1>
+    </div>
+  );
+}
+
+export default App;
